76a5b9285219d4630ad846278e0a0798dfeba500
Waseem Mohammad <wmohammad83@icloud.com>
2024-02-21 21:51:06 +0000

    Properties Page

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import properties from "@/properties.json";
+import PropertyCard from "@/components/PropertyCard";
 
 const PropertiesPage = () => {
-  return <div>Properties </div>;
+  return (
+    <section class="px-4 py-6">
+      <div class="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (
+          <div class="text-center">No properties found</div>
+        ) : (
+          <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property.id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default PropertiesPage;
