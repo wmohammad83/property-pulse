a1bd9223427e6010e06d6b344dc199b1518bf05f
Waseem Mohammad <wmohammad83@icloud.com>
2024-02-23 21:13:17 +0000

    Single Property Page

--- utils/request.js
+++ utils/request.js
@@ -1,5 +1,6 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
+// Fetch all properties
 async function fetchProperties() {
   try {
     // handle the case where the domain is not available yet
@@ -20,4 +21,25 @@ async function fetchProperties() {
   }
 }
 
-export { fetchProperties };
+//  Fetch single property
+async function fetchProperty(id) {
+  try {
+    // handle the case where the domain is not available yet
+    if (!apiDomain) {
+      return null;
+    }
+
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch properties");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export { fetchProperties, fetchProperty };
