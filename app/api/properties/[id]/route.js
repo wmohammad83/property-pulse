b9f2ac47a249278b074c296c0bc41f8bd9a6b7f0
Waseem Mohammad <wmohammad83@icloud.com>
2024-03-06 21:23:16 +0000

    Update PUT API Route

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -22,44 +22,6 @@ export const GET = async (request, { params }) => {
   }
 };
 
-// // DELETE /api/properties/:id
-// export const DELETE = async (request, { params }) => {
-//   try {
-//     const propertyId = params.id;
-
-//     const sessionUser = await getSessionUser();
-
-//     //  check for session
-//     if (!sessionUser || !sessionUser.userId) {
-//       return new Response("Unauthorized", { status: 401 });
-//     }
-
-//     const { userId } = sessionUser;
-
-//     await connectDB();
-
-//     const property = await Property.findById(propertyId);
-
-//     if (!property) {
-//       return new Response("Property not found", { status: 404 });
-//     }
-
-//     // check if the user is the owner of the property
-//     if (property.user.toString() !== userId) {
-//       return new Response("Unauthorized", { status: 401 });
-//     }
-
-//     await property.deleteOne();
-
-//     return new Response("Property Deleted", {
-//       status: 200,
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     return new Response("Something went wrong", { status: 500 });
-//   }
-// };
-
 // DELETE /api/properties/:id
 export const DELETE = async (request, { params }) => {
   try {
@@ -93,5 +55,72 @@ export const DELETE = async (request, { params }) => {
   } catch (error) {
     console.log(error);
     return new Response("Something Went Wrong", { status: 500 });
+  }
+};
+
+// PUT /api/properties/:id
+export const PUT = async (request, { params }) => {
+  try {
+    await connectDB();
+    const sessionUser = await getSessionUser();
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("Unauthorised", { status: 401 });
+    }
+
+    const { id } = params;
+    const { userId } = sessionUser;
+    const formData = await request.formData();
+
+    //  Access all values from amenities
+    const amenities = formData.getAll("amenities");
+
+    //  get property to update
+    const existingProperty = await Property.findById(id);
+
+    if (!existingProperty) {
+      return new Response("Property not found", { status: 404 });
+    }
+
+    //  Verify ownership
+    if (existingProperty.owner.toString() !== userId) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    // create a propertyData object to store in the database
+    const propertyData = {
+      type: formData.get("type"),
+      name: formData.get("name"),
+      description: formData.get("description"),
+      location: {
+        street: formData.get("location.street"),
+        city: formData.get("location.city"),
+        state: formData.get("location.state"),
+        zipcode: formData.get("location.zipcode"),
+      },
+      beds: formData.get("beds"),
+      baths: formData.get("baths"),
+      square_feet: formData.get("square_feet"),
+      amenities,
+      rates: {
+        weekly: formData.get("rates.weekly"),
+        monthly: formData.get("rates.monthly"),
+        nightly: formData.get("rates.nightly."),
+      },
+      seller_info: {
+        name: formData.get("seller_info.name"),
+        email: formData.get("seller_info.email"),
+        phone: formData.get("seller_info.phone"),
+      },
+      owner: userId,
+    };
+
+    //  Update the property
+    const updatedProperty = await Property.findByIdAndUpdate(id, propertyData);
+
+    return new Response(JSON.stringify(updatedProperty), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response("Failed to add property", { status: 500 });
   }
 };
